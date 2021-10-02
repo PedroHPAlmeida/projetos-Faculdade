b3c629cba8e7677e971c076378d13d664245042b
Pedro Henrique Pereira Almeida <pedro.6571almeida@gmail.com>
2021-10-02 17:25:28 -0300

    Implementação do conceito de Lista Ligada

--- 3_Semestre/Lista_Ligada/TesteLista.java
+++ 3_Semestre/Lista_Ligada/TesteLista.java
@@ -2,13 +2,27 @@ public class TesteLista {
     public static void main(String[] args) {
         Lista l = new Lista();
 
-        System.out.println(l);
+        System.out.println("lista criada:\n" + l);
 
         l.insereInicio(2);
         l.insereInicio(4);
         l.insereInicio(6);
         l.insereInicio(8);
+        System.out.println("lista depois das insercoes de inicio:\n" + l);
 
-        System.out.println(l);
+        l.insereFim(1);
+        l.insereFim(3);
+        l.insereFim(5);
+        System.out.println("lista depois das insercoes de fim:\n" + l);
+
+        if (!l.estaVazia()) {
+            System.out.println(l.removeInicio() + " foi removido do inicio da lista");
+            System.out.println("lista depois da remocao de inicio:\n" + l);
+        }
+
+        if (!l.estaVazia()) {
+            System.out.println(l.removeFim() + " foi removido do fim da lista");
+            System.out.println("lista depois da remocao de fim:\n" + l);
+        }
     }
 }
